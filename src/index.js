e62fe0f0ce33ccc27934480f934c7924a361a7fc
Mur <murmavilink@gmail.com>
2023-02-22 18:38:35 +0600

    lightbox functionality done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { modal } from "./modules/modal";
 import { slider } from "./modules/slider";
 import { timer } from "./modules/timer";
-import { validate } from "./modules/validate";
 import { sendForm } from "./modules/sendForm";
 
 modal('#header .fancyboxModal', '.header-modal', '.header-modal__close');
 modal('#services .fancyboxModal', '.services-modal', '.services-modal__close');
+
+modal('#documents .sertificate-document', '.sertificate__modal', '.sertificate__btn-close');
+
 slider('.benefits-inner', 3, '.benefits__arrow--left', '.benefits__arrow--right');
 slider('.services-inner', 2, '.services__arrow--left', '.services__arrow--right');
 timer('25 september 2023');
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,32 +1,48 @@
 export const modal = (selectorBtns, selectorModal, selectorModalClose, selectorOverlay = '.overlay') => {
-    
+
     const btns = document.querySelectorAll(selectorBtns);
-    const modal = document.querySelector(selectorModal); 
-    const modalClose = modal.querySelector(selectorModalClose); 
-    const overlay = document.querySelector(selectorOverlay); 
+    const modal = document.querySelector(selectorModal);
+    const modalClose = modal.querySelector(selectorModalClose);
+    const overlay = document.querySelector(selectorOverlay);
 
 
-    const addStyles = (blockOne, blockTwo, property = 'block') => {
-        blockOne.style.display = property;
-        blockTwo.style.display = property;
+    const addStyles = (blockOne, blockTwo, method, path) => {
+        if(path) {
+            blockOne.querySelector('img').setAttribute('src', path);
+        }
+
+        if(method === 'add') {
+            blockOne.classList.add( selectorModal.substring('1') + '--opened');
+            blockTwo.style.display = 'block';
+        } else if(method === 'remove') {
+            blockOne.classList.remove( selectorModal.substring('1') + '--opened');
+            blockTwo.style.display = 'none';
+        }
     };
 
-    
+
     btns.forEach(btn => {
         btn.addEventListener('click', (e) => {
             e.preventDefault();
-            addStyles(modal, overlay, 'block');
+            const pathImage = e.target.closest('a.sertificate-document');
+
+            if (pathImage) {
+                addStyles(modal, overlay, 'add', pathImage.getAttribute('href'));
+            } else {
+                addStyles(modal, overlay, 'add');
+            }
+
         });
     });
 
 
     modalClose.addEventListener('click', () => {
-        addStyles(modal, overlay, 'none');
+        addStyles(modal, overlay, 'remove');
     });
 
 
     overlay.addEventListener('click', () => {
-        addStyles(modal, overlay, 'none');
+        addStyles(modal, overlay, 'remove');
     });
 
 };
